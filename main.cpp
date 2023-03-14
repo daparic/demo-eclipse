911dfea5a2856421c32690c80b5a6170e9116838
truthadjuster <truthadjuster@gmail.com>
2023-03-14 14:03:19 +0800

    Add addnum, decide functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-int main(int argc, char *argv[])
+int addnum(int a, int b) {
+    return a + b;
+}
+
+bool decide(bool a, bool b, bool c) {
+    if (a && b && c) {
+        return true;
+    }
+    return false;
+}
+
+int main()
 {
     std::cout << "*** Parasoft C/C++test Demo ***\n";
     return 0;
