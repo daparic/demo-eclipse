c0709922b9ddcbb77e80e6a594a9a51ea2c52435
dx <dx@ubu22.04>
2023-07-26 02:16:21 +0800

    Add build scripts for arm, x86

---
+++ buildARM.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+
+make "CC=cpptestscan /opt/linaro/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc" CXX="cpptestscan /opt/linaro/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-g++"
