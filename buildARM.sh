7f60f0e71408011220fb9850b6e888dfa769ba5f
dx <dx@LAPTOP-1QQN8248>
2023-10-05 14:50:15 +0800

    Configure for Win11 helios

--- buildARM.sh
+++ buildARM.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #
-
-make "CC=cpptestscan /opt/linaro/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc" CXX="cpptestscan /opt/linaro/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-g++"
+L=/d/compilers/gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu
+P=/d/parasoft/2023/cpptest/professional-2023.1.0-win32.x86_64
+make "CC=cpptestscan aarch64-linux-gnu-gcc" CXX="cpptestscan aarch64-linux-gnu-g++"
